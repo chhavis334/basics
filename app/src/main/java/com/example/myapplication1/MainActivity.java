fabe705ca43d7280992e95a2df4bda3033f11959
Chhavi Saini <chhavisaini@Chhavis-MacBook-Pro.local>
2021-05-03 23:51:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b1;
+        EditText e,pw;
+        e=findViewById(R.id.editText);
+        pw=findViewById(R.id.editText2);
+
+
+        b1=findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication1/MainActivity2.java
@@ -0,0 +1,27 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button b2;
+        b2=findViewById(R.id.b1);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2=new Intent(MainActivity2.this,MainActivity3.class);
+                startActivity(intent2);
+
+            }
+        });
+    }
+}
